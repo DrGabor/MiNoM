b243bcbabad9e6b3a39ed5e6e7526ae947458d3c
Di Wang <35476470+DrGabor@users.noreply.github.com>
2020-02-27 22:15:21 +0800

    Update genParamsFun.m

--- genParamsFun.m
+++ genParamsFun.m
@@ -8,15 +8,17 @@ p.addParameter('Mov_Normal', []);
 p.addParameter('Ref_Normal', []);
 p.addParameter('optimize', 'irls'); 
 p.addParameter('Tf0', eye(4)); 
+%%%%%%%% P = [1.0 2.0] or [2.0 2.0], both are OK. 
 p.addParameter('P', [1.0 2.0]); 
 p.addParameter('maxIter_icp', 50);
 p.addParameter('maxEMCounter', 1);
-p.addParameter('maxIter_em', 10); % 20
-p.addParameter('maxIter_irls', 10); % 5
+%%%%% maxIter_em and maxIter_irls are very important. 
+p.addParameter('maxIter_em', 50);   % 20
+p.addParameter('maxIter_irls', 50); % 10
 p.addParameter('tf_eps', 1e-3);
 p.addParameter('IS_SHOW', 0);
 p.addParameter('verbose', 1);
-p.addParameter('bRand', logical(0) );
+p.addParameter('bRand', logical(1) );
 parser = p;
 parser.parse(varargin{:});
 params = parser.Results; 
