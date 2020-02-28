02527e62778f44c5eaef7bbcb4408c2389231b12
Di Wang <35476470+DrGabor@users.noreply.github.com>
2020-02-28 11:08:09 +0800

    Update main_demo.m

--- main_demo.m
+++ main_demo.m
@@ -1,14 +1,15 @@
 clc; close all;
-DataDir = 'data/raw_000802.pcd';
+DataDir = 'raw_000802.pcd';
 cloud_mov = pcread(DataDir);
-cloud_mov = pcdownsample(cloud_mov, 'random', 0.1); 
-DataDir = 'data/raw_000804.pcd';
+cloud_mov = pcdownsample(cloud_mov, 'random', 0.1);  % downsample for efficiency. 
+DataDir = 'raw_000804.pcd';
 cloud_ref = pcread(DataDir);
-params = genParamsFun(cloud_mov.Location', cloud_ref.Location', ...
-    'ref_normal', cloud_ref.Normal', ...
-    'P', [1.0 2.0], ...
-    'mode', 'point2plane', ...
+params = genParamsFun(cloud_mov.Location', ...  % 3XN matrix.
+     cloud_ref.Location', ...                   % 3XN matrix.
+    'ref_normal', cloud_ref.Normal', ...        % 3XN matrix. 
+    'P', [1.0 2.0], ...        % [2.0 2.0] can also be tried, but [1.0 2.0] seems to be more robust. 
+    'mode', 'point2plane', ... % 'point2plane' is robust than 'point2point' 
     'Tf0', eye(4), ...
-    'is_show', 1, ...
-    'verbose', 1 );
-[rstR, rstT] = MiNoMFun(params);
+    'is_show', 1, ... % is_show = 0 when use MiNoM in other applications.  
+    'verbose', 1 );   % verbose = 0 when use MiNoM in other applications. 
+[dR, dT] = MiNoMFun(params);
